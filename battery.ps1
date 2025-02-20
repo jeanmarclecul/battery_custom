3594e6b9aff3e0e2410ee167b21e1aa61f400f5f
Jean-Marc LECUL <jeanmarc.lecul.partner@decathlon.com>
2025-02-20 14:13:22 +0100

    feat(battery): no order needed

--- battery.ps1
+++ battery.ps1
@@ -25,22 +25,22 @@ $form.Controls.Add($label)
 
 # Allow form to be draggable
 $form.Add_MouseDown({
-    $form.Tag = $true
-    $form.Capture = $true
-    $point = [System.Drawing.Point]::new(0, 0)
-    [void][System.Windows.Forms.Cursor]::Position($point)
-})
+        $form.Tag = $true
+        $form.Capture = $true
+        $point = [System.Drawing.Point]::new(0, 0)
+        [void][System.Windows.Forms.Cursor]::Position($point)
+    })
 
 $form.Add_MouseMove({
-    if ($form.Tag -eq $true) {
-        $form.Location = [System.Windows.Forms.Cursor]::Position
-    }
-})
+        if ($form.Tag -eq $true) {
+            $form.Location = [System.Windows.Forms.Cursor]::Position
+        }
+    })
 
 $form.Add_MouseUp({
-    $form.Tag = $null
-    $form.Capture = $false
-})
+        $form.Tag = $null
+        $form.Capture = $false
+    })
 
 # Timer to update the label
 $timer = New-Object System.Windows.Forms.Timer
@@ -49,45 +49,45 @@ $rotateChars = @('\', '|', '/', '-')
 $global:charIndex = 0
 
 $timer.Add_Tick({
-    $StartTime = Get-Date
-    # $BTDeviceFriendlyNames = @("OPTIMA", "Philips TAT2206", "Peripheral2") # Add your list of peripherals here
-    $filePath = Join-Path -Path $PSScriptRoot -ChildPath "devices.txt"
-    $BTDeviceFriendlyNames = Get-Content -Path $filePath
-    $Messages = @()
+        $StartTime = Get-Date
+        $filePath = Join-Path -Path $PSScriptRoot -ChildPath "devices.txt"
+        $BTDeviceFriendlyNames = Get-Content -Path $filePath
+        $Messages = @()
+        $Messages += "BT>> "
 
-    foreach ($BTDeviceFriendlyName in $BTDeviceFriendlyNames) {
-        $BTHDevices = Get-PnpDevice -FriendlyName "*$($BTDeviceFriendlyName)*"
+        foreach ($BTDeviceFriendlyName in $BTDeviceFriendlyNames) {
+            $BTHDevices = Get-PnpDevice -FriendlyName "*$($BTDeviceFriendlyName)*"
 
-        if ($BTHDevices) {
-            $BatteryLevels = foreach ($Device in $BTHDevices) {
-                $BatteryProperty = Get-PnpDeviceProperty -InstanceId $Device.InstanceId -KeyName '{104EA319-6EE2-4701-BD47-8DDBF425BBE5} 2' |
-                Where-Object { $_.Type -ne 'Empty' } |
-                Select-Object -ExpandProperty Data
+            if ($BTHDevices) {
+                $BatteryLevels = foreach ($Device in $BTHDevices) {
+                    $BatteryProperty = Get-PnpDeviceProperty -InstanceId $Device.InstanceId -KeyName '{104EA319-6EE2-4701-BD47-8DDBF425BBE5} 2' |
+                    Where-Object { $_.Type -ne 'Empty' } |
+                    Select-Object -ExpandProperty Data
 
-                if ($BatteryProperty) {
-                    $BatteryProperty
+                    if ($BatteryProperty) {
+                        $BatteryProperty
+                    }
                 }
-            }
 
-            if ($BatteryLevels) {
-                $rotatingChar = $rotateChars[$global:charIndex]
-                $global:charIndex = ($global:charIndex + 1) % $rotateChars.Length
-                $Messages += "$($BTDeviceFriendlyName): $BatteryLevels % $rotatingChar"
-                Write-Host -NoNewline "`r$Message"
+                if ($BatteryLevels) {
+                    $rotatingChar = $rotateChars[$global:charIndex]
+                    $global:charIndex = ($global:charIndex + 1) % $rotateChars.Length
+                    $Messages += "$($BTDeviceFriendlyName): $BatteryLevels % $rotatingChar"
+                    Write-Host -NoNewline "`r$Message"
+                }
+                else {
+                    Write-Host -NoNewline "`r$Message"
+                    # $Messages += "$($BTDeviceFriendlyName) noBat."
+                }
             }
             else {
                 Write-Host -NoNewline "`r$Message"
-                # $Messages += "$($BTDeviceFriendlyName) noBat."
+                # $Messages += "$($BTDeviceFriendlyName). notFound"
             }
         }
-        else {
-            Write-Host -NoNewline "`r$Message"
-            # $Messages += "$($BTDeviceFriendlyName). notFound"
-        }
-    }
 
-    $label.Text = $Messages -join ' ' # [Environment]::NewLine
-})
+        $label.Text = $Messages -join ' ' # [Environment]::NewLine
+    })
 
 $form.Add_Shown({ $timer.Start() })
 $form.ShowDialog()
